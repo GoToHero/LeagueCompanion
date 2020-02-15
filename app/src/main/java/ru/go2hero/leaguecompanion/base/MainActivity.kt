ea2a2c762d439e33d58f8661494734e54489e05d
Go2Hero <andreysirotin20@gmail.com>
2020-02-15 23:49:16 +0600

    Added bottom bar navigation. Determined app main colors.

--- app/src/main/java/ru/go2hero/leaguecompanion/base/MainActivity.kt
+++ app/src/main/java/ru/go2hero/leaguecompanion/base/MainActivity.kt
@@ -1,14 +1,21 @@
 package ru.go2hero.leaguecompanion.base
 
+import android.os.Bundle
 import com.hannesdorfmann.mosby3.mvp.MvpActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.direct
-import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
-import org.kodein.di.generic.singleton
+import ru.go2hero.leaguecompanion.R
+import ru.go2hero.leaguecompanion.di.DI_LC_REPOSITORY
+import ru.go2hero.leaguecompanion.repository.LCRepository
 
 class MainActivity: MvpActivity<MainView, MainPresenter>(), KodeinAware, MainView {
+
+    private val USER_TAB: Int = 2
+    //private val repository: LCRepository = kodein.direct.instance(tag = DI_LC_REPOSITORY)
+
     override fun createPresenter(): MainPresenter {
         return kodein.direct.instance(tag = "mainPresenter")
     }
@@ -21,16 +28,38 @@ class MainActivity: MvpActivity<MainView, MainPresenter>(), KodeinAware, MainVie
         get() = Kodein.lazy {
             extend(parentKodein)
         }
-    //why we need backdoor interface?
-            /*import(Kodein.Module("backDoor") {
-                bind<BackDoor>(tag = "backdoorinterface") with singleton {
-                    return@singleton (object : BackDoor {
-                        override fun dismissAllDialogs() {
-                            this@KidmostActivity.dismissAllDialogs()
-                        }
-                    })
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViewElements()
+    }
+
+    private fun initViewElements() {
+        main_container.offscreenPageLimit = 1
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        //TODO Add screens
+        //adapter.addFragment()
+        main_container.adapter = adapter
+    }
+
+    private fun showTab(position: Int) {
+        main_container.currentItem = position
+    }
+
+
+    private fun checkExistUserProfile() {
+        //TODO If user data exist -> show user profile
+
+        //TODO query user profile image for bottom bar
+
+        bnv.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_profile -> {
+                    //if (supportFragmentManager.findFragmentById(R.id.main_container)  )
+                    showTab(USER_TAB)
                 }
-            })*/
-
-
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
 }
